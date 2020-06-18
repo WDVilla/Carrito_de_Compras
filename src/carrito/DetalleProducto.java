c34089e36ae6860e22b7a2280923dec10b70e321
diego <diego@mbp1517ved>
2020-06-18 11:36:15 -0300

    primer commit de carrito

---
+++ src/carrito/DetalleProducto.java
@@ -0,0 +1,16 @@
+
+public class DetalleProducto {
+	private int cantidad;
+	private Producto prod;
+	public DetalleProducto() {
+	}
+	public void setProducto(Producto p) {
+		prod = p;
+	}
+	public void setCantidad(int c) {
+		cantidad = c;
+	}
+	public Producto getProducto(){
+		return prod;
+	}
+}
